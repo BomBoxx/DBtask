e09c37522c6f4e2f22c0708280d7fb4aa54170fb
BomBoxx <abdelrahmanmarwan52@gmail.com>
2023-01-26 17:04:37 +0200

    creating the employee table

--- DBtask/Salaries.Designer.cs
+++ DBtask/Salaries.Designer.cs
@@ -67,9 +67,9 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.FloralWhite;
-            this.button2.Location = new System.Drawing.Point(309, 513);
+            this.button2.Location = new System.Drawing.Point(309, 425);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 281);
+            this.button2.Size = new System.Drawing.Size(148, 166);
             this.button2.TabIndex = 43;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = false;
@@ -80,9 +80,9 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(309, 236);
+            this.button1.Location = new System.Drawing.Point(309, 250);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 272);
+            this.button1.Size = new System.Drawing.Size(148, 153);
             this.button1.TabIndex = 42;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
